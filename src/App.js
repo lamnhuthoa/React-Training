0d9490a888d3fa2c4bd487db0a0bc6387c6431a9
lamnhuthoa <hoalamnhut@gmail.com>
2021-06-24 15:08:37 +0700

    BaiTapThucHanhLayout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 // import FunctionComponent from "./Components/FunctionComponent";
 // import ClassComponent from "./Components/ClassComponent";
 // import HomeComponent from './Components/LayoutExercise/HomeComponent';
-import Databinding from './Databinding/Databinding';
+// import Databinding from './Databinding/Databinding';
+import BaiTapThucHanhLayout from './Components/BaiTapLayoutComponent/BaiTapThucHanhLayout'
 
 function App() {
   return (
     <div className="App">
       {/* <HomeComponent/> */}
-      <Databinding/>
+      {/* <Databinding/> */}
+      <BaiTapThucHanhLayout/>
     </div>
   );
 }
---
+++ src/Components/BaiTapLayoutComponent/Item.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+export default class Item extends Component {
+    render() {
+        return (
+            <div>
+                <section className="pt-4">
+                    <div className="container px-lg-5">
+                        <div className="row gx-lg-5">
+                            <div className="col-lg-3 col-xxl-4 mb-5">
+                                <div className="card bg-light border-0 h-100">
+                                    <img class="card-img-top" src="https://via.placeholder.com/500x325.png?text=500+x+325" alt="Card cap"/>
+                                    <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                        <h2 className="fs-4 fw-bold">Card title</h2>
+                                        <p className="mb-0">With Bootstrap 5, we've created a fresh new layout for this template!</p>
+                                    </div>
+                                    <div className="card-footer text-center">
+                                        <button className="btn btn-primary">Find Out More!</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-3 col-xxl-4 mb-5">
+                                <div className="card bg-light border-0 h-100">
+                                    <img class="card-img-top" src="https://via.placeholder.com/500x325.png?text=500+x+325" alt="Card cap"/>
+                                    <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                        <h2 className="fs-4 fw-bold">Card title</h2>
+                                        <p className="mb-0">As always, Start Bootstrap has a powerful collectin of free templates.</p>
+                                    </div>
+                                    <div className="card-footer text-center">
+                                        <button className="btn btn-primary">Find Out More!</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-3 col-xxl-4 mb-5">
+                                <div className="card bg-light border-0 h-100">
+                                    <img class="card-img-top" src="https://via.placeholder.com/500x325.png?text=500+x+325" alt="Card cap"/>
+                                    <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                        <h2 className="fs-4 fw-bold">Card title</h2>
+                                        <p className="mb-0">The heroic part of this template is the jumbotron hero header!</p>
+                                    </div>
+                                    <div className="card-footer text-center">
+                                        <button className="btn btn-primary">Find Out More!</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-3 col-xxl-4 mb-5">
+                                <div className="card bg-light border-0 h-100">
+                                    <img class="card-img-top" src="https://via.placeholder.com/500x325.png?text=500+x+325" alt="Card cap"/>
+                                    <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                        <h2 className="fs-4 fw-bold">Card title</h2>
+                                        <p className="mb-0">We've created some custom feature boxes using Bootstrap icons!</p>
+                                    </div>
+                                    <div className="card-footer text-center">
+                                        <button className="btn btn-primary">Find Out More!</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+        )
+    }
+}
